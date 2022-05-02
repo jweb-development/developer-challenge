bf951231dcf76c8eff27197eb5a9dd20b970a9df
miguel bastidas <m.alejandro_80@hotmail.com>
2022-05-02 15:21:20 -0500

    task number 1 solved

--- src/lib/db/users.js
+++ src/lib/db/users.js
@@ -1,13 +1,13 @@
+/** @format */
+
 const { db } = require('../utils')
 
 const users = {
   getUsers: async () => {
     try {
       const userQuery = `
-      SELECT u.userID, u.firstName, u.lastName, u.isActive, u.createdDate, r.name as roleName
+       SELECT u.userID, u.firstName, u.lastName, u.isActive, u.createdDate, r.name as roleName
         FROM user u
-        JOIN user_login l
-          ON u.userID = l.userID
         JOIN user_role ur
           ON ur.userID = u.userID
         JOIN role r
@@ -16,7 +16,9 @@ const users = {
 
       const userRes = await db.query(userQuery)
 
-      if (userRes) { return userRes }
+      if (userRes) {
+        return userRes
+      }
 
       return false
     } catch (err) {
@@ -24,7 +26,7 @@ const users = {
       return false
     }
   },
-  getUser: async (userID) => {
+  getUser: async userID => {
     try {
       const userQuery = `
       SELECT u.userID, u.firstName, u.lastName, u.isActive, u.createdDate, r.name as roleName
@@ -40,7 +42,9 @@ const users = {
 
       const userRes = await db.query(userQuery)
 
-      if (userRes) { return userRes }
+      if (userRes) {
+        return userRes
+      }
 
       return false
     } catch (err) {
