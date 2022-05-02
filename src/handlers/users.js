51a92aea7470deb55ee0f5927a5d66ad7ffd94d8
miguel bastidas <m.alejandro_80@hotmail.com>
2022-05-02 16:03:50 -0500

    tasks 2 and 3

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require('express')
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
@@ -11,13 +13,18 @@ const app = express()
 app.use(cors())
 
 const defaultFileParser = bodyParser.json({ extended: true, limit: '500kb' })
-const defaultFileEncoder = bodyParser.urlencoded({ extended: true, limit: '500kb' })
+const defaultFileEncoder = bodyParser.urlencoded({
+  extended: true,
+  limit: '500kb'
+})
 
 // List users
 app.get('/v1/user', defaultFileParser, defaultFileEncoder, async (req, res) => {
   try {
     const results = await users.getUsers()
-    if (!results) { return responseHandler.handleNotFound('users', req, res) }
+    if (!results) {
+      return responseHandler.handleNotFound('users', req, res)
+    }
 
     const response = {
       data: results,
@@ -33,26 +40,52 @@ app.get('/v1/user', defaultFileParser, defaultFileEncoder, async (req, res) => {
 })
 
 // GET user by ID
-app.get('/v1/user/:userID', defaultFileParser, defaultFileEncoder, async (req, res) => {
-  try {
-    const id = req.params.userID
+app.get(
+  '/v1/user/:userID',
+  defaultFileParser,
+  defaultFileEncoder,
+  async (req, res) => {
+    try {
+      const id = req.params.userID
 
-    const results = users.getUser(id)
+      const results = await users.getUser(id)
 
-    if (!results) { return responseHandler.handleNotFound('user', req, res) }
+      if (!results) {
+        return responseHandler.handleNotFound('user', req, res)
+      }
 
-    const response = {
-      data: results,
-      message: `Supervisor ${results.fullName} found.`
+      const response = {
+        data: results,
+        message: `Supervisor ${results[0].firstName}  ${results[0].lastName} found.`
+      }
+
+      return responseHandler.handleSuccess(response, req, res)
+    } catch (err) {
+      console.error(err.message)
+      const response = { data: null, message: 'Internal Server Error' }
+      return responseHandler.handleError(response, req, res)
     }
-
-    return responseHandler.handleUnauthorized(req, res)
-  } catch (err) {
-    console.error(err.message)
-    const response = { data: null, message: 'Internal Server Error' }
-    return responseHandler.handleError(response, req, res)
   }
-})
+)
+
+app.delete(
+  '/v1/user/:userID',
+  defaultFileParser,
+  defaultFileEncoder,
+  async (req, res) => {
+    try {
+      const id = req.params.userID
+
+      const results = await users.deleteUser(id)
+
+      return responseHandler.handleSuccess(results, req, res)
+    } catch (err) {
+      console.error(err.message)
+      const response = { data: null, message: 'Internal Server Error' }
+      return responseHandler.handleError(response, req, res)
+    }
+  }
+)
 
 // Handle in-valid route
 app.all('*', function (req, res) {
--- src/lib/db/users.js
+++ src/lib/db/users.js
@@ -29,15 +29,32 @@ const users = {
   getUser: async userID => {
     try {
       const userQuery = `
-      SELECT u.userID, u.firstName, u.lastName, u.isActive, u.createdDate, r.name as roleName
+        SELECT u.userID, u.firstName, u.lastName, u.isActive, u.createdDate, r.name as roleName
         FROM user u
-        JOIN user_login l
-          ON u.userID = l.userID
         JOIN user_role ur
           ON ur.userID = u.userID
         JOIN role r
           ON r.roleID = ur.roleID
-        WHERE userID = '${userID}'
+        WHERE u.userID = '${userID}'
+      `
+
+      const userRes = await db.query(userQuery)
+
+      if (userRes) {
+        return userRes
+      }
+
+      return false
+    } catch (err) {
+      console.log(err)
+      return false
+    }
+  },
+
+  deleteUser: async userID => {
+    try {
+      const userQuery = `
+        DELETE FROM user WHERE UserID ='${userID}'
       `
 
       const userRes = await db.query(userQuery)
